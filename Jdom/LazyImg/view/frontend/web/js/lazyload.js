e8271b93c35b3903575f509c51fec326d667183f
jonathan072 <38061317+jonathan072@users.noreply.github.com>
2024-11-17 22:47:06 -0400

    Update lazyload.js

--- Jdom/LazyImg/view/frontend/web/js/lazyload.js
+++ Jdom/LazyImg/view/frontend/web/js/lazyload.js
@@ -1,12 +1,13 @@
 //(Chrome / Firefox / Opera / Edge)
 function newdefer() {
-    var lazyloadImages;
+
+    let lazyloadImages;
     if ("IntersectionObserver" in window) {
         lazyloadImages = document.querySelectorAll("img");
-        var imageObserver = new IntersectionObserver(function(entries, observer) {
+        let imageObserver = new IntersectionObserver(function(entries, observer) {
             entries.forEach(function(entry) {
                 if (entry.isIntersecting) {
-                    var image = entry.target;
+                    let image = entry.target;
                     if (image.dataset.origen) {
                         image.src = image.dataset.origen;
                         imageObserver.unobserve(image);
@@ -18,14 +19,14 @@ function newdefer() {
             imageObserver.observe(image);
         });
     } else {
-        var lazyloadThrottleTimeout;
+        let lazyloadThrottleTimeout;
         lazyloadImages = document.querySelectorAll("img");
         function lazyload() {
             if (lazyloadThrottleTimeout) {
                 clearTimeout(lazyloadThrottleTimeout);
             }
             lazyloadThrottleTimeout = setTimeout(function() {
-                var scrollTop = window.pageYOffset;
+                let scrollTop = window.pageYOffset;
                 lazyloadImages.forEach(function(img) {
                     if (img.offsetTop < (window.innerHeight + scrollTop)) {
                         img.src = img.dataset.origen;
