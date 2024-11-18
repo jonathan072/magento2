799c8381cbc1f3192edd22cc8cb578a1ed82a22a
jonathan072 <38061317+jonathan072@users.noreply.github.com>
2024-11-17 21:40:47 -0400

    Create BlockPlugin.php

---
+++ Jdom/LazyImg/plugin/BlockPlugin.php
@@ -0,0 +1,42 @@
+<?php
+namespace Jdom\LazyImg\Plugin;
+
+class BlockPlugin
+{
+    protected $request;
+    protected $scopeConfig;
+
+    /**
+     * Constructor for BlockPlugin class.
+     *
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        \Magento\Framework\App\RequestInterface $request,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    ) {
+        $this->request = $request;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function afterToHtml(\Magento\Framework\View\Element\AbstractBlock $block, $html)
+    {
+        $isEnabled = $this->scopeConfig->getValue('jdom_lazyimg/general/enabled', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+
+        if (!$isEnabled) {
+            return $html;
+        }
+
+        $imagen = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNiYAAAAAkAAxkR2eQAAAAASUVORK5CYII=';
+
+        $pixelSrc = 'src="' . $imagen . '"';
+        $tmpSrc = 'TMP_SRC';
+        $html = str_replace($pixelSrc, $tmpSrc, $html);
+        $html = preg_replace('#<img\s+([^>]*)(?:src="([^"]*)")([^>]*)\/?>#isU', '<img ' . $pixelSrc . ' data-origen="$2" $1 $3/>', $html);
+        $html = str_replace($tmpSrc, $pixelSrc, $html);
+
+        return $html;
+    }
+}
+?>
