e04164ddbdbc1f52d35af83f1eaf73c095222e7c
jonathan072 <38061317+jonathan072@users.noreply.github.com>
2024-11-19 21:00:42 -0400

    Update BlockPlugin.php

--- Jdom/LazyImg/Plugin/BlockPlugin.php
+++ Jdom/LazyImg/Plugin/BlockPlugin.php
@@ -1,43 +1,76 @@
 <?php
 namespace Jdom\LazyImg\Plugin;
 
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Element\AbstractBlock;
+use Magento\Store\Model\ScopeInterface;
+use Psr\Log\LoggerInterface;
+
 class BlockPlugin
 {
     protected $request;
     protected $scopeConfig;
-
+    protected $logger;
     /**
      * Constructor for BlockPlugin class.
      *
-     * @param \Magento\Framework\App\RequestInterface $request
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param RequestInterface $request
+     * @param ScopeConfigInterface $scopeConfig
+     * @param LoggerInterface $logger
      */
     public function __construct(
-        \Magento\Framework\App\RequestInterface $request,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        RequestInterface $request,
+        ScopeConfigInterface $scopeConfig,
+        LoggerInterface $logger
     ) {
         $this->request = $request;
         $this->scopeConfig = $scopeConfig;
+        $this->logger = $logger;
     }
-
-    public function afterToHtml(\Magento\Framework\View\Element\AbstractBlock $block, $html)
+    /**
+     * Modifies the HTML of blocks after rendering.
+     * @param AbstractBlock $block
+     * @param string $html
+     * @return string
+     */
+    public function afterToHtml(AbstractBlock $block, $html)
     {
-        $isEnabled = $this->scopeConfig->getValue('jdom_lazyimg/general/enabled', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $isEnabled = $this->scopeConfig->getValue(
+            'jdom_lazyimg/general/enabled',
+            ScopeInterface::SCOPE_STORE
+        );
 
+        // Return original HTML if the module is not enabled
         if (!$isEnabled) {
             return $html;
         }
 
-        $imagen = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNiYAAAAAkAAxkR2eQAAAAASUVORK5CYII=';
+        try {
+            $placeholderImage = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNiYAAAAAkAAxkR2eQAAAAASUVORK5CYII=';
+            $placeholderSrc = 'src="' . $placeholderImage . '"';
+            $tempSrcPlaceholder = 'TMP_SRC';
 
-        $pixelSrc = 'src="' . $imagen . '"';
-        $tmpSrc = 'TMP_SRC';
-      
-        $html = str_replace($pixelSrc, $tmpSrc, $html);
-        $html = preg_replace('#<img\s+([^>]*)(?:src="([^"]*)")([^>]*)\/?>#isU', '<img ' . $pixelSrc . ' data-origen="$2" $1 $3/>', $html);
-        $html = str_replace($tmpSrc, $pixelSrc, $html);
+            // Temporarily replace src to avoid conflicts during processing
+            $html = str_replace($placeholderSrc, $tempSrcPlaceholder, $html);
 
-        return $html;
+            // Add data-origen attribute and set placeholder image
+            $html = preg_replace(
+                '#<img\s+([^>]*)(?:src="([^"]*)")([^>]*)\/?>#isU',
+                '<img ' . $placeholderSrc . ' data-source="$2" $1 $3/>',
+                $html
+            );
+
+            // Restore placeholder src
+            $html = str_replace($tempSrcPlaceholder, $placeholderSrc, $html);
+
+            return $html;
+            } catch (\Exception $e) {
+            // Log the exception for debugging purposes
+            $this->logger->error('Error in LazyImg BlockPlugin: ' . $e->getMessage(), ['exception' => $e]);
+
+            // Return the original HTML in case of an error
+            return $html;
+        }
     }
 }
-?>
